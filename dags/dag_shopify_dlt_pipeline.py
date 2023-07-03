9f1abe1a434db65c5e7b61464e1a4208220700b3
Zaeem Athar <zaeemathar94@gmail.com>
2023-07-03 13:42:34 +0200

    Editing dag to get a single source.

--- dags/dag_shopify_dlt_pipeline.py
+++ dags/dag_shopify_dlt_pipeline.py
@@ -46,19 +46,19 @@ def load_shopify_data():
     # except activities from the beginning
     source = shopify_source()
 
-    # # Another source configured to activities
-    # # starting at the given date (custom_fields_mapping is included to
-    # # translate custom field hashes to names)
-    # activities_source = shopify_source(
-    #     since_timestamp="2023-03-01 00:00:00Z"
-    # ).with_resources("activities")
+    # Another source configured to activities
+    # starting at the given date (custom_fields_mapping is included to
+    # translate custom field hashes to names)
+    customer_source = shopify_source(
+        since_timestamp="2023-03-01 00:00:00Z"
+    ).with_resources("customers")
 
     # Create the source, the "serialize" decompose option
     # will convert dlt resources into Airflow tasks.
     # Use "none" to disable it.
     tasks.add_run(
         pipeline=pipeline,
-        data=source,
+        data=customer_source,
         decompose="serialize",
         trigger_rule="all_done",
         retries=0,
