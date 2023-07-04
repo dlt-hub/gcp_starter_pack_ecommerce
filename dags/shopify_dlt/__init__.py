c9dd904ca1b1ee0076ee622f475089a47d2fdb31
Zaeem Athar <zaeemathar94@gmail.com>
2023-07-04 21:35:45 +0200

    Geting all orders.

--- dags/shopify_dlt/__init__.py
+++ dags/shopify_dlt/__init__.py
@@ -74,7 +74,7 @@ def shopify_source(
             Iterable[TDataItem]: A generator of orders.
         """
         page = shopify.Order.find(
-            updated_at_min=updated_at.last_value, created_at_min=start_date
+            updated_at_min=updated_at.last_value, created_at_min=start_date, status="any"
         )
         yield iterate_page(page)
 
