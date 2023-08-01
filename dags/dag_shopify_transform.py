a9195487bab7464a447d0f5fb6142d7dc4de1fa8
Zaeem Athar <zaeemathar94@gmail.com>
2023-08-02 00:15:18 +0200

    Adding seperate task.

--- dags/dag_shopify_transform.py
+++ dags/dag_shopify_transform.py
@@ -1,5 +1,4 @@
-import dlt
-from airflow.decorators import dag
+from airflow.decorators import dag, task
 from dlt.common import pendulum
 
 # modify the dag arguments
@@ -20,20 +19,28 @@ default_args = {
     catchup=False,
     default_args=default_args
 )
-def shopify_dbt():
-    import os
-    from dlt.common.runners import Venv
-    
-    pipeline = dlt.pipeline(pipeline_name='shopify', destination='bigquery', dataset_name='shopify_data')
-    # now that data is loaded, let's transform it
-    # make or restore venv for dbt, uses latest dbt version
-    venv = Venv.restore_current()
-    # get runner, optionally pass the venv
-    here = os.path.dirname(os.path.realpath(__file__))
-    dbt = dlt.dbt.package(pipeline, os.path.join(here, "../transform/shopify_dbt"),
-        venv=venv)
-    models = dbt.run_all()
-    for m in models:
-        print(f"Model {m.model_name} materialized in {m.time} with status {m.status} and message {m.message}")
+def transform():
+    """
+    The Transform Dag
+    """
+    @task
+    def shopify_dbt():
+        import dlt
+        import os
+        from dlt.common.runners import Venv
 
-shopify_dbt()
+        pipeline = dlt.pipeline(pipeline_name='shopify', destination='bigquery', dataset_name='shopify_data')
+        # now that data is loaded, let's transform it
+        # make or restore venv for dbt, uses latest dbt version
+        venv = Venv.restore_current()
+        # get runner, optionally pass the venv
+        here = os.path.dirname(os.path.realpath(__file__))
+        dbt = dlt.dbt.package(pipeline, os.path.join(here, "../transform/shopify_dbt"),
+            venv=venv)
+        models = dbt.run_all()
+        for m in models:
+            print(f"Model {m.model_name} materialized in {m.time} with status {m.status} and message {m.message}")
+
+    shopify_dbt()
+
+transform()
